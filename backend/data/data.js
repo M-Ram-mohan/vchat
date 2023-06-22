527cb6369aa13425533b3c4b2313f0373610dff5
M-Ram-mohan <mrm12@iitbbs.ac.in>
2023-06-22 13:43:08 +0530

    Created Express Server and tested APIs using Postman

---
+++ backend/data/data.js
@@ -0,0 +1,34 @@
+const chats = [
+    {
+        isGroupChat: false,
+        users: [
+            {
+                name: "John",
+                email: "john@example.com",
+            },
+            {
+                name: "Ram",
+                email: "ram@example.com"
+            }
+        ],
+        _id: "12345",
+        chatName: "John", 
+    },
+    {
+        isGroupChat: false,
+        users: [
+            {
+                name: "July",
+                email: "july@example.com",
+            },
+            {
+                name: "Ram",
+                email: "ram@example.com"
+            }
+        ],
+        _id: "12346",
+        chatName: "July", 
+    },
+];
+
+module.exports = { chats };
